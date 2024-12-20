850a46aa5a6357b585e2b27308ce027d55f0bab5
LorenzoLollis <lorenzo_marino_roma@hotmail.com>
2024-12-20 12:49:49 +0100

    tostring e filtro data

--- es DiarioDiBordo/Diario.cs
+++ es DiarioDiBordo/Diario.cs
@@ -7,6 +7,7 @@ using _04_Utility;
 
 namespace es_DiarioDiBordo
 {
+
     internal class Diario : Entity
     {
         public DateTime Data { get; set; }
@@ -15,40 +16,28 @@ namespace es_DiarioDiBordo
 
         public float CordinataY { get; set; }
 
-        public string Luogo {  get; set; }
+        public string Luogo { get; set; } = string.Empty;
 
-        public string Descrizione { get; set; }
+        public string Descrizione { get; set; } = string.Empty;
 
 
-        public Diario(int id, DateTime data, float cordinataX, float cordinataY, string luogo, string descrizione)
-            : base(id)
+        public override string ToString()
         {
-            Data = data;
-            CordinataX = cordinataX;
-            CordinataY = cordinataY;
-            Luogo = luogo;
-            Descrizione = descrizione;
+            return base.ToString() +
+                $"Data: {Data}\n" +
+                $"CordinataX: {CordinataX}\n" +
+                $"CordinataY: {CordinataY}\n" +
+                $"Luogo: {Luogo}\n" +
+                $"Descrizione: {Descrizione}\n" +
+                $"--------------------------------";
         }
 
-        public string ToString()
-        {
-            return "Data: " + Data + " CordinataX: " + CordinataX + " CordinataY: " + CordinataY + " Luogo: " + Luogo + " Descrizione: " + Descrizione;
-        }
-
-
-        /*
-            Ricerca in base a un periodo di tempo (Esempio: tra 01-01-2019 e 01-01-2020)
-            Ricerca in base al luogo
-            Ricerca in base a una parte della descrizione
-        */
-
-        public static List<DiarioEntry> CercaPerPeriodo(List<DiarioEntry> dati, DateTime inizio, DateTime fine)
+        public static List<DiarioEntry> CercaPerPeriodo(List<DiarioEntry> dati, Date inizio, Date fine)
         {
             // Filtra i dati in base al periodo specificato
             return dati.Where(entry => entry.Data >= inizio && entry.Data <= fine).ToString();
         }
 
-
-
     }
+
 }
